ffa6ebd2735897ed4723693db5c8b57e06d07893
dharmendrasha <dharmendra.soni@scaletech.xyz>
2024-08-08 13:41:10 +0530

    fix: cloudwatch load module

--- public-api-task-fargate/main.tf
+++ public-api-task-fargate/main.tf
@@ -81,8 +81,7 @@ resource "aws_iam_role_policy" "ecs_task_execution_policy" {
 
 
 module "cloudwatch_logger" {
-  source  = "git::https://github.com/dataminer-site/aws-platform-engineering.git//cloudwatch?ref=main"
-  version = "0.0.1"
+  source  = "../cloudwatch"
   config = {
     environment       = var.environment
     context           = var.context
