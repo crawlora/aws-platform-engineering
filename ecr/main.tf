8714d964706ae00b86a36ba28cd76c2ca3e4c1e3
dharmendrasha <dharmendra.soni@scaletech.xyz>
2024-08-12 19:54:31 +0530

    fix: tag mutability

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "repository" {
   name                 = var.name
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = var.is_mutable ? "MUTABLE" : "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = false
   }
--- ecr/variables.tf
+++ ecr/variables.tf
@@ -4,3 +4,10 @@ variable "name" {
 variable "tags" {
   type = map(string)
 }
+
+
+variable "is_mutable" {
+  default = true
+  type = bool
+  description = "tag mutability MUTABLE | IMMUTABLE @read: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository"
+}
